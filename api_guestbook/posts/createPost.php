7520e8610b27b23ae2d0864a4e32afdd3d00ee95
teacarolina <teacarolina@icloud.com>
2021-03-18 17:45:16 +0100

    fix

--- api_guestbook/posts/createPost.php
+++ api_guestbook/posts/createPost.php
@@ -3,5 +3,5 @@ include("../db.php");
 include("Post.php");
 
 $post = new Post($pdo);
-$post->CreatePost("Emilia", "emilia@testing.com", "Jag testar en ny sak för att se om det fungerar");
+$post->CreatePost("name", "email", "message");
 ?>
